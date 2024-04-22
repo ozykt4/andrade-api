ff4936e9f8e8c2a00dded0d8ad272ec700fe4269
Ozy Kleyton <kleytonb8@gmail.com>
2024-04-22 11:34:22 -0300

    ✨ add user handler

--- internal/api/handler/user.go
+++ internal/api/handler/user.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/ozykt4/andrade-api/internal/api/router"
 	"github.com/ozykt4/andrade-api/internal/model"
@@ -21,6 +23,10 @@ func (uh *UserHandler) Routes() router.Router {
 	return func(router fiber.Router) {
 		user := router.Group("users")
 		user.Post("/", uh.CreateUserHandler)
+		user.Get("/", uh.FindAllUsersHandler)
+		user.Get("/:id", uh.FindUserByID)
+		user.Put("/:id", uh.UpdateUserHandler)
+		user.Delete("/:id", uh.DeleteUserHandler)
 	}
 }
 
@@ -32,5 +38,37 @@ func (uh *UserHandler) CreateUserHandler(ctx *fiber.Ctx) error {
 	}
 
 	res := uh.service.CreateUser(userReq)
+	return ctx.Status(res.Status).JSON(res)
+}
+
+func (uh *UserHandler) FindAllUsersHandler(ctx *fiber.Ctx) error {
+	res := uh.service.FindAllUsers()
+	return ctx.Status(res.Status).JSON(res)
+}
+
+func (uh *UserHandler) FindUserByID(ctx *fiber.Ctx) error {
+	userID, _ := strconv.Atoi(ctx.Params("id", "0"))
+
+	res := uh.service.FindUserByID(uint(userID))
+
+	return ctx.Status(res.Status).JSON(res)
+}
+
+func (uh *UserHandler) UpdateUserHandler(ctx *fiber.Ctx) error {
+	userReq := new(model.UserReq)
+	if err := ctx.BodyParser(userReq); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(model.NewErrorResponse(err, fiber.ErrBadRequest))
+	}
+
+	userID, _ := strconv.Atoi(ctx.Params("id", "0"))
+
+	res := uh.service.UpdateUser(uint(userID), userReq)
+	return ctx.Status(res.Status).JSON(res)
+}
+
+func (uh *UserHandler) DeleteUserHandler(ctx *fiber.Ctx) error {
+	userID, _ := strconv.Atoi(ctx.Params("id", "0"))
+
+	res := uh.service.DeleteUser(uint(userID))
 	return ctx.Status(res.Status).JSON(res)
 }
