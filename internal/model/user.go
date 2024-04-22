00d558db4e0e9a1da0c1328d2d6bc3d454da4759
Ozy Kleyton <kleytonb8@gmail.com>
2024-04-22 10:16:37 -0300

    ✨ add User model

---
+++ internal/model/user.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name     string
+	Email    string
+	Password string
+}
+
+type UserReq struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserRes struct {
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAT time.Time `json:"created_at"`
+	UpdatedAT time.Time `json:"updated_at"`
+}
+
+func (u *UserReq) ToUser() *User {
+	return &User{
+		Name:     u.Name,
+		Email:    u.Email,
+		Password: u.Password,
+	}
+}
+
+func (ur *User) ToUserRes() *UserRes {
+	return &UserRes{
+		Name:      ur.Name,
+		Email:     ur.Email,
+		CreatedAT: ur.CreatedAt,
+		UpdatedAT: ur.UpdatedAt,
+	}
+}
